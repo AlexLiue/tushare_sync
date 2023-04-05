176e23c19457d759ea9c6b18e083d5135cb8ac08
alex <alexliue@outlook.com>
2023-04-05 22:37:45 +0800

    添加从断点日期继续同步功能

--- tables/stk_rewards/stk_rewards.py
+++ tables/stk_rewards/stk_rewards.py
@@ -33,7 +33,7 @@ def exec_sync(start_date, end_date):
             "reward",
             "hold_vol"
         ],
-        date_column='end_date',
+        date_column='ann_date',
         start_date=start_date,
         end_date=end_date,
         date_step=1,
@@ -51,7 +51,7 @@ def sync(drop_exist):
     # 查询历史最大同步日期
     begin_date = '19940803'
     cfg = get_cfg()
-    date_query_sql = "select max(end_date) date from %s.stk_rewards" % cfg['mysql']['database']
+    date_query_sql = "select max(ann_date) date from %s.stk_rewards" % cfg['mysql']['database']
     last_date = query_last_sync_date(date_query_sql)
     start_date = max_date(last_date, begin_date)
     end_date = str(datetime.datetime.now().strftime('%Y%m%d'))
